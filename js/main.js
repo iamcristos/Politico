da30bb033cf8098c83eb9037b8fd9749c53893b7
iamcristos <nmereginivincent@gmail.com>
2019-01-25 14:34:43 +0000

    second commit

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ explorebtn.addEventListener('click', ()=>{
     location.href='userLogin.html'
 });
 
-let form= document.getElementsByClassName('form-group')
+let form= document.getElementsByClassName('form-group');
+
+
+let nav = document.getElementById('visible');
+let navFunction= ()=>{
+    let navItems= document.getElementsByClassName('right');
+    // navItems.forEach(item=>{
+    //     item.classList.toggle('show');
+    // });
+    for(var i = 0; i < navItems.length; i++){
+        navItems[i].classList.toggle('show');
+    }
+}
+
+nav.addEventListener('click', navFunction);
---
+++ test.js
@@ -0,0 +1,11 @@
+alert('ok')
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  document.querySelector('.visible').addEventListener('click', classToggle)
+
+  function navShow() {
+      const 
+  }
